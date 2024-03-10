112257764180a1733922e3eaf5c30071e8c01cc2
Coderkainat <159674299+Coderkainat@users.noreply.github.com>
2024-03-10 22:36:21 +0500

    Add files via upload

---
+++ Q5/Q5.ts
@@ -0,0 +1,8 @@
+// Q5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+
+let famous_person: string ="Hazrat Ali"
+
+let message: string ="Accept the Apology,Even if it is not sincere"
+
+console.log(`${famous_person} said ${message}`)
